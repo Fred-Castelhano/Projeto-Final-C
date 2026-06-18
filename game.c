a5fa2a7d57af90cc3d738d2aa75f4831375b2cb3
Fred <fredcastelhano@gmail.com>
2026-06-18 23:04:06 +0100

    [Sessão 1] Estrutura inicial do projeto

---
+++ game.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "game.h"
+
+void desenharMapa() {
+    printf("--- Jogo Flicky (Versao Terminal) ---\n");
+    // Aqui vamos colocar a lógica da matriz mais tarde
+}
---
+++ game.h
@@ -0,0 +1,9 @@
+#ifndef GAME_H
+#define GAME_H
+
+#define LARGURA 20
+#define ALTURA 10
+
+void desenharMapa();
+
+#endif
